cfe0facc3b0577550363a299aa7309e86695ef41
George <george.ailenei@gmail.com>
2022-10-17 17:03:36 +0100

    working on the relationship between users and cardroom

--- whist/admin.py
+++ whist/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from whist.models import CardRoom
 
 # Register your models here.
+admin.site.register(CardRoom)
--- whist/models.py
+++ whist/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from userauth.models import User
+
+
+# not sure if this is ok yet.
+class CardRoom(models.Model):
+    name = models.CharField(max_length=60)
+    username = models.OneToOneField(
+        User, on_delete=models.CASCADE,
+        primary_key=True
+    )
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=60)
     hand = models.CharField(max_length=50)
     tricks = models.IntegerField()
     played_cards = models.CharField(max_length=50)
