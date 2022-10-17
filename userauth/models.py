1aba53fc810a2217c006258ef55c229bc45bc304
George <george.ailenei@gmail.com>
2022-10-17 14:25:57 +0100

    still working on the customer user model

--- userauth/models.py
+++ userauth/models.py
@@ -1,46 +1,46 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=40, unique=True)
-    data_of_birth = models.DateField(unique=True)
-    email = models.EmailField()
+class User(AbstractUser, PermissionsMixin):
+    pass
+    # username = models.CharField(max_length=40, unique=True)
+    # data_of_birth = models.DateField(unique=True)
+    # email = models.EmailField()
+    #
+    # USERNAME_FIELD = 'username'
+    # REQUIRED_FIELDS = ['data_of_birth', 'email']
+    #
+    # is_staff = models.BooleanField(default=False)
+    # is_active = models.BooleanField(default=False)
+    #
+    # def __str__(self):
+    #     return self.username
+    #
 
-    USERNAME_FIELD = 'username'
-    EMAIL_FIELD = email
-    REQUIRED_FIELDS = ['data_of_birth']
+# class CustomUserManager(BaseUserManager):
+    # def create_user(self, username, date_of_birth, email, password, **other_fields):
+    #     if not email:
+    #         raise ValueError('Users must have an email address')
+    #
+    #     user = self.model(
+    #         username=username,
+    #         email=self.normalize_email(email),
+    #         date_of_birth=date_of_birth,
+    #         **other_fields
+    #     )
+    #     user.set_password(password)
+    #     user.save(using=self._db)
+    #     return user
 
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.username
-
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, date_of_birth, email, password=None, **other_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
-            **other_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, date_of_birth, email, password, **other_fields):
-        other_fields.setdefault('is_staff', True)
-        other_fields.setdefault('is_superuser', True)
-        other_fields.setdefault('is_activate', True)
-
-        if other_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must be assigner to is_staff=True.')
-        if other_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must be assigner to is_superuser=True.')
-
-        return self.create_user(username, date_of_birth, email, password)
+    # def create_superuser(self, username, email, password, **other_fields):
+    #     other_fields.setdefault('is_staff', True)
+    #     other_fields.setdefault('is_superuser', True)
+    #     other_fields.setdefault('is_activate', True)
+    #
+    #     if other_fields.get('is_staff') is not True:
+    #         raise ValueError('Superuser must be assigner to is_staff=True.')
+    #     if other_fields.get('is_superuser') is not True:
+    #         raise ValueError('Superuser must be assigner to is_superuser=True.')
+    #
+    #     return self.create_user(username, email, password)
--- website/settings.py
+++ website/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+STATIC_URL = '/src/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'src',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
