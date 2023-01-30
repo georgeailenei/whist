2074bd8fff6574cd26aea8bd8e6e8c3147702ba3
George Ailenei <george.ailenei@gmail.com>
2023-01-30 11:52:46 +0000

    rank system added

---
+++ src/profiles/controllers/rank_controller.py
@@ -0,0 +1,13 @@
+from userauth.models import User
+
+
+def ranking(user):
+    users = User.objects.all().order_by('points').values()
+    position = 1
+
+    for u in users:
+        if user.username == u['username']:
+            return position
+        else:
+            position += 1
+            
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from .controllers.rank_controller import ranking
 
 
 class UserProfile(LoginRequiredMixin, TemplateView):
     template_name = 'profiles/profile.html'
 
     def get(self, request, **kwargs):
-        return render(request, self.template_name)
+        user = request.user
+        rank = ranking(user)
 
+        context = {
+            'rank': rank,
+        }
+
+        return render(request, self.template_name, context)
