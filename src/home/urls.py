a234cd4d0607da2c9337f3f1c28adafccab3d6c7
George <george.ailenei@gmail.com>
2023-02-02 11:52:18 +0000

    website styling

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from room.views import CardRooms
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home_page"),
     path("card_rooms", CardRooms.as_view(), name="card_rooms"),
-]
+] + staticfiles_urlpatterns()
