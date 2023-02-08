1faf39d61c798df504daa3f21a8ec6791ff3ff16
George Ailenei <george.ailenei@gmail.com>
2023-02-08 23:21:32 +0000

    logo & game styling

--- whist_ui/src/App.vue
+++ whist_ui/src/App.vue
@@ -3,13 +3,8 @@ import Controller from './components/Controller.vue';
 </script>
 
 <template>
-<div class="main-container">
 <Controller />
-</div>
 </template>
 
 <style scoped>
-.main-container{
-  background-color: #333333;;
-}
 </style>
